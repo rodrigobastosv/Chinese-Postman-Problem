f629ad3fdddfc0d24fe79d85278d83702f1a119f
Rodrigo Bastos <rodrigobastosv@gmail.com>
2016-11-01 21:45:41 -0200

    Adicionando configurações

--- src/config/Configurations.java
+++ src/config/Configurations.java
@@ -5,4 +5,6 @@ import javafx.scene.paint.Color;
 public class Configurations {
 
 	public static Color EDGES_COLOR;
+	public static String BOLD_STYLE = "-fx-font-weight: bold;";
+	public static String WHITE_BACKGROUND = "-fx-background: #FFFFFF;";
 }
