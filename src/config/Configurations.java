e7c9eae32137b20eba659adc827b29ec4c3ab102
Rodrigo Bastos <rodrigobastosv@gmail.com>
2016-11-06 16:29:46 -0200

    Novas configurações com a altura e largura da tela

--- src/config/Configurations.java
+++ src/config/Configurations.java
@@ -4,6 +4,8 @@ import javafx.scene.paint.Color;
 
 public class Configurations {
 
+	public static int WINDOW_HEIGHT = 300; 
+	public static int WINDOW_WIDHT = 400;
 	public static Color EDGES_COLOR;
 	public static float EDGES_THICKNESS;
 	public static String BOLD_STYLE = "-fx-font-weight: bold;";
