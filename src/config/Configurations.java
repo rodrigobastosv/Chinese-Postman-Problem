ec695530753f07c97c5a34264b1ea411db3bd8f2
Rodrigo Bastos <rodrigobastosv@gmail.com>
2016-11-07 22:09:55 -0200

    Adicionando novas configs

--- src/config/Configurations.java
+++ src/config/Configurations.java
@@ -4,6 +4,8 @@ import javafx.scene.paint.Color;
 
 public class Configurations {
 
+	public static int VERTEX_COUNT;
+	public static int INITIAL_VERTEX;
 	public static int WINDOW_HEIGHT = 300; 
 	public static int WINDOW_WIDHT = 400;
 	public static Color EDGES_COLOR;
@@ -12,4 +14,8 @@ public class Configurations {
 	public static String WHITE_BACKGROUND = "-fx-background: #FFFFFF;";
 	public static float MIN_THICKNESS = 1F;
 	public static float MAX_THICKNESS = 3F;
+	
+	public static void setVertexCount(int vertexCount) {
+		VERTEX_COUNT = vertexCount;
+	}
 }
